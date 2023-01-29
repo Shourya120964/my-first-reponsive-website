e924fb142db1545a613f2d9b8e35df0a4ddfc8a1
Anshu <anshuverma2100@gmail.com>
2023-01-29 12:13:58 +0000

    burger menu fixed

--- Js/resp.js
+++ Js/resp.js
@@ -1,12 +1,12 @@
 burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 NavList = document.querySelector('.nav-list')
-rightNav = document.querySelector('.rightNav')
+rightNav = document.querySelector('.right-nav')
 
 
 burger.addEventListener('click', ()=>{
     rightNav.classList.toggle('v-class-resp');
-    navList.classList.toggle('v-class-resp');
+    NavList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
 
 })
